eed878dab1634075722422f38d56999db7dd46c5
Prudhvi Rampey <gergteg777@gmail.com>
2017-04-11 12:21:34 +0530

    Initial commit with teleport feature implemented

---
+++ js/crawling-cursor.js
@@ -0,0 +1,71 @@
+/**
+ * Created by prudhvirampey on 11/04/17.
+ */
+/* global AFRAME */
+
+if (typeof AFRAME === 'undefined') {
+    throw new Error('Component attempted to register before AFRAME was available.');
+}
+
+/**
+ * Crawling Cursor component for A-Frame.
+ */
+AFRAME.registerComponent('crawling-cursor', {
+    dependencies: ['raycaster'],
+    schema: {
+        target: {
+            type: "selector"
+        }
+    },
+
+    multiple: false,
+
+    init: function() {
+        var el = this.el;
+        var data = this.data;
+
+        if (data.target === null) {
+            var cursor = document.querySelector("a-cursor");
+
+            if (cursor === null) {
+                console.warn("Please put a-cursor in a document");
+                return;
+            }
+
+            data.target = cursor;
+        }
+
+        el.addEventListener("raycaster-intersection", function(e) {
+
+            var intersection = getNearestIntersection(e.detail.intersections);
+            if (!intersection) {return;}
+
+            // a matrix which represents item's movement, rotation and scale on global world
+            var mat = intersection.object.matrixWorld;
+            // remove parallel movement from the matrix
+            mat.setPosition(new THREE.Vector3(0, 0, 0));
+
+            // change local normal into global normal
+            var global_normal = intersection.face.normal.clone().applyMatrix4(mat).normalize();
+
+            // look at target coordinate = intersection coordinate + global normal vector
+            var lookAtTarget = new THREE.Vector3().addVectors(intersection.point, global_normal);
+            data.target.object3D.lookAt(lookAtTarget);
+            this.emit("gaze_fixed",{ gaze_coordinates: lookAtTarget },true);
+            // cursor coordinate = intersection coordinate + normal vector * 0.05(hover 5cm above intersection point)
+            var cursorPosition = new THREE.Vector3().addVectors(intersection.point, global_normal.multiplyScalar(0.05));
+            data.target.setAttribute("position", cursorPosition);
+
+            function getNearestIntersection(intersections) {
+                for (var i = 0, l = intersections.length; i < l; i++) {
+
+                    // ignore cursor itself to avoid flicker && ignore "ignore-ray" class
+                    if (data.target === intersections[i].object.el || intersections[i].object.el.classList.contains("ignore-ray")) {continue;}
+                    return intersections[i];
+                }
+                return null;
+            }
+        });
+
+    }
+});
---
+++ js/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by prudhvirampey on 11/04/17.
+ */
+import 'aframe';
+// Some weird bug where I can't seem to import js files from the same folder as main.js
+import '../js/crawling-cursor.js';
