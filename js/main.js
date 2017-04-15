a314ecb30b3d738f346ba2f8c76f139bc683d980
Prudhvi Rampey <gergteg777@gmail.com>
2017-04-15 18:59:49 +0530

    updated package

--- js/main.js
+++ js/main.js
@@ -4,3 +4,6 @@
 import 'aframe';
 // Some weird bug where I can't seem to import js files from the same folder as main.js
 import '../js/crawling-cursor.js';
+var extras= require('aframe-extras');
+extras.registerAll();
+
