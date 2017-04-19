6a2e7ad392de2830fd5a7d903b576eba323da7a3
Prudhvi Rampey <gergteg777@gmail.com>
2017-04-19 20:52:04 +0530

    Enabled visual path tracing

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-/**
- * Created by prudhvirampey on 11/04/17.
- */
 import 'aframe';
 // Some weird bug where I can't seem to import js files from the same folder as main.js
-import '../js/crawling-cursor.js';
+import './crawling-cursor.js';
 var extras= require('aframe-extras');
 extras.registerAll();
+// Component for drawing path curves
