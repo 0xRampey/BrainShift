3ec6093a448435ba0e7bd1e4007cc29af3a9f94a
Prudhvi Rampey <prudhvi.rampey@gmail.com>
2017-08-16 14:03:38 +0530

    Interfaced neurosky with error reporting
    Code refactored so that web socket events can easily call position changes.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var express = require('express');
 var io = require('socket.io')(http);
 var http = require('http').Server(app);
+// Setting up library for interfacing with Neurosky
+var neurosky = require('./js/neurosky');
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -19,3 +21,22 @@ io.on('connection', function (socket) {
 
 io.listen(app.listen(3000));
 console.log("Listening for users on port 3000");
+
+// Creating connection with Thinkgear
+var client = neurosky.createClient({
+	appName: 'BrainShift',
+	appKey: '0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
+});
+
+client.on('data',function(data){
+
+	console.log(data);
+});
+
+client.on('connect_error', function(err) {
+  console.log("Thinkgear seems to be offline. Please check connection");
+});
+
+console.log("Attempting connection with Thinkgear....");
+client.connect();
+
--- js/neurosky.js
+++ js/neurosky.js
@@ -1,14 +1,67 @@
+var net = require('net'),
+	events = require('events'),
+	util = require('util');
 
-// Setting up library for interfacing with Neurosky
-var neurosky = require('node-neurosky');
-var client = neurosky.createClient({
-	appName: 'BrainShift',
-	appKey: '0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
+function NodeThinkGearError(message){
+  Error.call(this);
+  Error.captureStackTrace(this, arguments.callee);
+  this.message = message;
+  this.name = 'NodeThinkGearError';
+};
+
+NodeThinkGearError.prototype.__proto__ = Error.prototype;
+
+var ThinkGearClient = function(opts){
+	opts || (opts = {});
+
+	this.port = opts.port || 13854;
+	this.host = opts.host || 'localhost';
+
+	if(typeof(opts.appName) !== 'string') throw new NodeThinkGearError('Must specify appName');
+	if(typeof(opts.appKey) !== 'string') throw new NodeThinkGearError('Must specify appKey');
+	
+	this.auth = {
+		appName:opts.appName,
+		appKey:opts.appKey
+	};
+
+	this.config = {
+		enableRawOutput: false,
+		format: "Json"
+	};
+
+	events.EventEmitter.call(this);
+};
+
+util.inherits(ThinkGearClient, events.EventEmitter);
+
+ThinkGearClient.prototype.connect = function(){
+	var self = this;
+	
+	var client = this.client = net.connect(this.port,this.host,function(){
+		client.write(JSON.stringify(self.auth));
+	});
+	
+	client.on('data',function(data){
+		if(!self.configSent){
+			self.configSent = true;
+			client.write(JSON.stringify(self.config));
+		} else {
+			try{
+				self.emit('data',JSON.parse(data.toString()));
+			}catch(e){
+				self.emit('parse_error',data.toString());
+			}
+		}
+	});
+
+	client.on('error', function(err) {
+		self.emit('connect_error', err);
 });
+};
 
-client.on('data',function(data){
+exports.ThinkGearClient = ThinkGearClient;
 
-	console.log(data);
-});
-
-client.connect();
+exports.createClient = function(opts){
+	return new ThinkGearClient(opts || {});
+};
